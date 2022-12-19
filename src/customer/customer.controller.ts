a2a57ff87c79f0ac0a19ddf91986a1a8dcf687b7
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2022-12-19 19:16:43 -0300

    end lesson 3

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CustomerService } from './customer.service';
 
 @Controller('customer')
 export class CustomerController {
   constructor(private customerService: CustomerService) {}
 
-  @Get()
-  getCustomer() {
-    this.customerService.findCustomer();
+  @Get(':id')
+  getCustomer(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.customerService.findCustomerById(id);
+    if (!customer) {
+      throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST);
+    }
+    return customer;
   }
 }
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -2,7 +2,24 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomerService {
-  findCustomer() {
-    return 'Customer';
+  private customers = [
+    {
+      id: 1,
+      name: 'John Doe',
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      name: 'John Smith',
+      createdAt: new Date(),
+    },
+  ];
+  findCustomerById(id: number) {
+    return this.customers.find((customer) => customer.id === id);
   }
 }
