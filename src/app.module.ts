1059cba0f8fb095eea7d21041cbe8f75b3292a72
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2023-01-02 16:37:42 -0300

    using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './config/typeorm.config';
 import { CustomerModule } from './customer/customer.module';
+import getPassword from './gitignorethis';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CustomerModule, UserModule],
+  imports: [
+    CustomerModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '192.168.0.100',
+      port: 5432,
+      username: 'victor',
+      password: getPassword(),
+      database: 'victordb',
+      entities: entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/user/user.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+    name: 'user_id',
+  })
+  id: number;
+
+  @Column({
+    nullable: false,
+    default: '',
+    unique: true,
+  })
+  username: string;
+
+  @Column({
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+    name: 'email_address',
+  })
+  emailAddress: string;
+}
