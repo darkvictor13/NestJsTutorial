730a86fcb9b99ac1c7b5fd915ba1b357ca33a696
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2023-01-03 21:49:44 -0300

    fixing user crud to use db

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,6 +11,7 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { CreateUserDto } from './dtos/create_user.dto';
 import { UserNotFoundException } from './exceptions/user_not_found.exception';
 import { UserHttpExceptionFilter } from './filters/user_http_exception.filter';
@@ -22,28 +23,32 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getUsers(): SerializedUser[] {
-    return this.userService.getUsers();
+  async getUsers() {
+    return plainToInstance(SerializedUser, await this.userService.getUsers());
   }
 
   @Get('name/:username')
   @UseFilters(UserHttpExceptionFilter)
-  getUserByUsername(@Param('username') username: string): SerializedUser {
-    const user = this.userService.getUserByUsername(username);
+  async getUserByUsername(
+    @Param('username') username: string,
+  ): Promise<SerializedUser> {
+    const user = await this.userService.getUserByUsername(username);
     if (!user) {
       throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
     }
-    return user;
+    return plainToInstance(SerializedUser, user);
   }
 
   @Get('id/:id')
   @UseFilters(UserHttpExceptionFilter)
-  getUserById(@Param('id', ParseIntPipe) id: number): SerializedUser {
-    const user = this.userService.getUserById(id);
+  async getUserById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<SerializedUser> {
+    const user = await this.userService.getUserById(id);
     if (!user) {
       throw new UserNotFoundException();
     }
-    return user;
+    return plainToInstance(SerializedUser, user);
   }
 
   @Post()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,62 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { SerializedUser } from './types/serialized_user';
-import { User } from './types/user';
 import { CreateUserDto } from './dtos/create_user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User as UserEntity } from './user.entity';
+import { User } from './user.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [
-    {
-      id: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      id: 2,
-      username: 'chris',
-      password: 'secret',
-    },
-    {
-      id: 3,
-      username: 'maria',
-      password: 'guess',
-    },
-    {
-      id: 4,
-      username: 'admin',
-      password: 'admin',
-    },
-  ];
-
   constructor(
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  getUsers(): SerializedUser[] {
-    return plainToInstance(SerializedUser, this.users);
+  getUsers(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  getUserByUsername(username: string): SerializedUser {
-    for (const user of this.users) {
-      if (user.username === username) {
-        return plainToInstance(SerializedUser, user);
-      }
-    }
-    return null;
+  getUserByUsername(username: string): Promise<User> {
+    return this.userRepository.findOne({
+      where: {
+        username,
+      },
+    });
   }
 
-  getUserById(id: number): SerializedUser {
-    for (const user of this.users) {
-      if (user.id === id) {
-        return plainToInstance(SerializedUser, user);
-      }
-    }
-    return null;
+  getUserById(id: number): Promise<User> {
+    return this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
   }
 
   createUser(createUserDto: CreateUserDto) {
